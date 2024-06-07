ceaa416dd9dbb0db1de16d80950cfd1ecd53cf5d
Teo Lambarelli <56316971+Teo-Lambarelli@users.noreply.github.com>
2024-06-07 16:19:59 +0200

    Json added

---
+++ src/SafeManager.java
@@ -0,0 +1,10 @@
+import battlemanager.Team;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+
+public class SafeManager {
+	static public void safeFile(Team team) {
+		
+	}
+}
